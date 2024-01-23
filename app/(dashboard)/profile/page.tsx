37e34ae1ea22ea9610513177ad83cfc8e24ee136
J2DevStudio <j2devstudio@gmail.com>
2024-01-22 20:29:34 -0500

    react query setup

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -1,4 +1,17 @@
-const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+//import { fetchUserTokensById } from "@/utils/actions";
+import { UserProfile, auth } from "@clerk/nextjs";
+
+const ProfilePage = async () => {
+  const { userId } = auth();
+  //const currentTokens = await fetchUserTokensById(userId);
+  return (
+    <div>
+      <h2 className="mb-8 ml-8 text-xl font-extrabold">
+        {/* Token Amount : {currentTokens} */}
+        Token Amount: 0
+      </h2>
+      <UserProfile />
+    </div>
+  );
 };
 export default ProfilePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import Providers from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -20,7 +20,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Providers>{children}</Providers>
+        </body>
       </html>
     </ClerkProvider>
   );
