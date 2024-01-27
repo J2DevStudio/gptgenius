5b0ac10be5676d6e31182563010e18fcc76ec3f1
J2DevStudio <j2devstudio@gmail.com>
2024-01-26 19:42:46 -0500

    OpenAI Chat - Added context

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -3,19 +3,42 @@
 import { useMutation } from "@tanstack/react-query";
 import { generateChatResponse } from "@/utils/actions";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 const Chat = () => {
   const [text, setText] = useState("");
-  const [message, setMessage] = useState([]);
+  const [messages, setMessages] = useState([]);
 
   const { mutate } = useMutation({
-    mutationFn: (message) => generateChatResponse(message),
+    mutationFn: async (query) => {
+      //const currentTokens = await fetchUserTokensById(userId);
+
+      // if (currentTokens < 100) {
+      //   toast.error("Token balance too low....");
+      //   return;
+      // }
+
+      const response = await generateChatResponse([...messages, query]);
+
+      if (!response) {
+        toast.error("Something went wrong...");
+        return;
+      }
+      setMessages((prev) => [...prev, response.message]);
+      //const newTokens = await subtractTokens(userId, response.tokens);
+      //toast.success(`${newTokens} tokens remaining...`);
+    },
   });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    mutate(text);
+    const query = { role: "user", content: text };
+    mutate(query);
+    setMessages((prev) => [...prev, query]);
+    setText("");
   };
+
+  console.log(messages);
 
   const isPending = false;
 
