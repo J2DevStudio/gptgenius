bcb98aca796fc73c643e3e00be7a3965c5a1ea66
J2DevStudio <j2devstudio@gmail.com>
2024-01-26 20:36:51 -0500

    render chat interface

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -9,7 +9,7 @@ const Chat = () => {
   const [text, setText] = useState("");
   const [messages, setMessages] = useState([]);
 
-  const { mutate } = useMutation({
+  const { mutate, isPending } = useMutation({
     mutationFn: async (query) => {
       //const currentTokens = await fetchUserTokensById(userId);
 
@@ -38,16 +38,27 @@ const Chat = () => {
     setText("");
   };
 
-  //console.log(messages);
+  console.log(messages);
   //will show sent and received responses, and a history for context
   //as an array of objects, each object containing [role] and [content]
-
-  const isPending = false;
 
   return (
     <div className="min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto]">
       <div>
-        <h2 className="text-5xl">messages</h2>
+        {messages.map(({ role, content }, index) => {
+          const avatar = role == "user" ? "👤" : "🤖";
+          const bcg = role === "user" ? "bg-base-200" : "bg-base-100";
+          return (
+            <div
+              key={index}
+              className={`${bcg} flex py-6 -mx-8 px-8 text-xl leading-loose border-b border-base-300`}
+            >
+              <span className="mr-4">{avatar}</span>
+              <p className="max-w-3xl">{content}</p>
+            </div>
+          );
+        })}
+        {isPending ? <span className="loading"></span> : null}
       </div>
       <form onSubmit={handleSubmit} className="max-w-4xl pt-12">
         <div className="join w-full">
@@ -62,7 +73,7 @@ const Chat = () => {
           <button
             className="btn btn-primary join-item"
             type="submit"
-            // disabled={isPending}
+            disabled={isPending}
           >
             {isPending ? "please wait..." : "ask question"}
           </button>
