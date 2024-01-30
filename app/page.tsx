6d74c958b9b1f5f79b10b0e243a4daf0375f9578
J2DevStudio <j2devstudio@gmail.com>
2024-01-30 13:01:17 -0500

    To Production

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ const HomePage = () => {
           <UserButton afterSignOutUrl="/" />
           <h1 className="text-6xl font-bold text-primary">GPTGenius </h1>
           <p className="py-6 text-lg leading-loose">
-            GPTGenius: Your AI language companion. Powered by OpenAI, it
-            enhances your conversations, content creation, and more!
+            Your AI language companion. Powered by OpenAI, it enhances your
+            conversations, content creation, and more!
           </p>
           <Link href="/chat" className="btn btn-secondary">
             Get Started
